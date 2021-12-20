5f178de4bab2d54fea907f387ee28cd3948a3a72
pajang1515 <73331335+dongmyoungLee@users.noreply.github.com>
2021-12-20 16:01:45 +0900

    preventDefault

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const loginForm = document.querySelector('#login-form')
+const loginInput = document.querySelector('#login-form input')
+const greeting = document.querySelector('#greeting')
+
+const HIDEEN_CLASSNAME = 'hidden'
+
+function onLoginSubmit(e) {
+    e.preventDefault()
+    loginForm.classList.add(HIDEEN_CLASSNAME)
+    const username = loginInput.value;
+    greeting.innerText = `Hello ${username}`
+    greeting.classList.remove(HIDEEN_CLASSNAME)
+}
+
+loginForm.addEventListener('submit', onLoginSubmit)
+
