217026d9f7b8e7bb741ad381d470d91bc4034fdd
pajang1515 <73331335+dongmyoungLee@users.noreply.github.com>
2021-12-21 15:22:18 +0900

    21/12/21

--- js/app.js
+++ js/app.js
@@ -3,14 +3,26 @@ const loginInput = document.querySelector('#login-form input')
 const greeting = document.querySelector('#greeting')
 
 const HIDEEN_CLASSNAME = 'hidden'
+const USERNAME_KEY = 'username'
 
 function onLoginSubmit(e) {
     e.preventDefault()
     loginForm.classList.add(HIDEEN_CLASSNAME)
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username)
+    paintGreethings(username)
+}
+
+function paintGreethings(username) {
     greeting.innerText = `Hello ${username}`
     greeting.classList.remove(HIDEEN_CLASSNAME)
 }
 
-loginForm.addEventListener('submit', onLoginSubmit)
+const saveUsername = localStorage.getItem(USERNAME_KEY)
 
+if (saveUsername ===  null) {
+    loginForm.classList.remove(HIDEEN_CLASSNAME)
+    loginForm.addEventListener('submit', onLoginSubmit)
+} else {
+    paintGreethings(saveUsername)
+}
